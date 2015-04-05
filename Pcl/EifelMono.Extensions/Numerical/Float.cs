5c2f0831bfde9be69ada9b0ffa1f79f75ad52b2d
Andreas Klapperich <andreas@klapperich.de>
2015-04-05 09:05:38 +0200

    NEW: Math Abs, Min, Max NEW: SafeInvoke Action<_, T1 .. T5>, Func<_, T1 .. T5> DEL: Net 3.5 removed (MONO 4.0)

---
+++ Pcl/EifelMono.Extensions/Numerical/Float.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace EifelMono.Extensions
+{
+    public static partial class NumericalExtensions
+    {
+        #region Math
+
+        public static float Abs(this float value)
+        {
+            return Math.Abs(value);
+        }
+
+        public static float Min(this float value, float otherValue)
+        {
+            return Math.Min(value, otherValue);
+        }
+
+        public static float Max(this float value, float otherValue)
+        {
+            return Math.Max(value, otherValue);
+        }
+
+        #endregion
+    }
+}
