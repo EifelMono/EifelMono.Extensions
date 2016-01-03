c310ac6c1511f13cf11857721130da3954a4099d
Andreas Klapperich <eifelmono@klapperch.de>
2016-01-03 14:50:11 +0100

    CHG: Auto NuGet

--- EifelMono.Extensions.nuspec.command
+++ EifelMono.Extensions.nuspec.command
@@ -4,6 +4,8 @@ echo "cd-ing to $here"
 cd "$here" || exit 1
 xbuild /p:Configuration=Debug EifelMono.Extensions.sln
 xbuild /p:Configuration=Release EifelMono.Extensions.sln
-mono NuGet.exe pack EifelMono.Extensions.nuspec
-// mono NuGet.exe setApiKey 54d97fc4-b428-4b72-a212-54124778c643
-// mono NuGet.exe push xxx.nupkg
+mono EifelMono.Nuget.exe nuspecversionfromdll $here/Pcl/EifelMono.Extensions/bin/Release/EifelMono.Extensions.dll
+rm *.nupkg
+mono NuGet.exe pack EifelMono.Extensions.nuspec 
+mono NuGet.exe setApiKey 54d97fc4-b428-4b72-a212-54124778c643
+mono NuGet.exe push *.nupkg
--- Pcl/EifelMono.Extensions/Properties/AssemblyInfo.cs
+++ Pcl/EifelMono.Extensions/Properties/AssemblyInfo.cs
@@ -18,8 +18,6 @@ using System.Runtime.CompilerServices;
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
 [assembly: AssemblyVersion(EifelMono.Extensions.AssemblyInfoProperties.Version)]
-[assembly: AssemblyFileVersion(EifelMono.Extensions.AssemblyInfoProperties.Version)]
-[assembly: AssemblyInformationalVersion(EifelMono.Extensions.AssemblyInfoProperties.Version)]
 
 // The following attributes are used to specify the signing key for the assembly,
 // if desired. See the Mono documentation for more information about signing.
@@ -31,6 +29,6 @@ namespace EifelMono.Extensions
 {
     class AssemblyInfoProperties
     {
-        public const string Version= "2.2.0.0";
+        public const string Version= "2.1.1";
     }
 }
