46d4a2ae4d497b03557805f2b22a532ad73eb467
Andreas Klapperich <eifelmono@klapperch.de>
2016-02-13 18:53:39 +0100

    ICHG: First with Template added

--- #Test/EifelMono.Extensions.Test/TestBoolean.cs
+++ #Test/EifelMono.Extensions.Test/TestBoolean.cs
@@ -29,6 +29,71 @@ namespace EifelMono.Extensions.Test
             Assert.IsFalse(running.IsFirst);
             Assert.IsFalse(running.IsFirst);
         }
+
+        [Test()]
+        public void TestBoolean2()
+        {
+            var running = new First<string>();
+
+            Assert.IsTrue(running.IsFirst);
+            Assert.IsFalse(running.IsFirst);
+            Assert.IsFalse(running.IsFirst);
+            Assert.IsFalse(running.IsFirst);
+            Assert.IsFalse(running.IsFirst);
+
+            running.Reset();
+
+            Assert.IsTrue(running.IsFirst);
+            Assert.IsFalse(running.IsFirst);
+            Assert.IsFalse(running.IsFirst);
+            Assert.IsFalse(running.IsFirst);
+            Assert.IsFalse(running.IsFirst);
+
+            Assert.IsTrue(string.IsNullOrEmpty(running.Value));
+            Assert.IsTrue(string.IsNullOrEmpty(running.DefaultValue));
+
+            running.Value = "abc";
+            Assert.IsTrue(running.Value == "abc");
+            running.Reset();
+            Assert.IsTrue(string.IsNullOrEmpty(running.DefaultValue));
+            Assert.IsTrue(running.Value == "abc");
+            running.Reset(true);
+            Assert.IsTrue(string.IsNullOrEmpty(running.Value));
+            Assert.IsTrue(string.IsNullOrEmpty(running.DefaultValue));
+        }
+
+
+        [Test()]
+        public void TestBoolean3()
+        {
+            var running = new First<string>("abcde");
+
+            Assert.IsTrue(running.IsFirst);
+            Assert.IsFalse(running.IsFirst);
+            Assert.IsFalse(running.IsFirst);
+            Assert.IsFalse(running.IsFirst);
+            Assert.IsFalse(running.IsFirst);
+
+            running.Reset();
+        
+            Assert.IsTrue(running.IsFirst);
+            Assert.IsFalse(running.IsFirst);
+            Assert.IsFalse(running.IsFirst);
+            Assert.IsFalse(running.IsFirst);
+            Assert.IsFalse(running.IsFirst);
+
+            Assert.IsTrue(running.Value == "abcde");
+            Assert.IsTrue(running.DefaultValue == "abcde");
+
+            running.Value = "abc";
+            Assert.IsTrue(running.Value == "abc");
+            running.Reset();
+            Assert.IsTrue(running.Value == "abc");
+            Assert.IsTrue(running.DefaultValue== "abcde");
+            running.Reset(true);
+            Assert.IsTrue(running.Value == "abcde");
+            Assert.IsTrue(running.DefaultValue == "abcde");
+        }
     }
 }
 
