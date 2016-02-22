cbc4361fe6d9ddc26e7fa980738bfe9ff6e12b2d
Andreas Klapperich <eifelmono@klapperch.de>
2016-02-22 21:37:49 +0100

    NEW: Run.Execute(async()=> {doSomeThing;});

---
+++ Pcl/EifelMono.Extensions/Flow/RunExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace EifelMono.Extensions
+{
+    public static class Run
+    {
+        /// <summary>
+        /// Execute the specified action.
+        /// Run.Execute(async()=> {doSomeThing;});
+        /// </summary>
+        /// <param name="action">Action.</param>
+        public static void Execute(Action action)
+        {
+            action();
+        }
+    }
+}
+
