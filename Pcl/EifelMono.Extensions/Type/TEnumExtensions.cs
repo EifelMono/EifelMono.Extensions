1ace4e0f5126bd64c5eea2bec9f5fb923a4db4fe
Andreas Klapperich <eifelmono@klapperch.de>
2016-02-25 16:14:13 +0100

    NEW: using static EifelMono.Extensions.Static for ....... NEW: foreach (var x in Enumerate<ENUM>())

---
+++ Pcl/EifelMono.Extensions/Type/TEnumExtensions.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace EifelMono.Extensions
+{
+    public static class TEnumExtensions
+    {
+        public static IEnumerable<T> EnumerateAllValues<T>(this T value)
+        {
+            #if NOPCL
+            if (!typeof(T).IsEnum)
+            #else
+            if (!typeof(T).GetTypeInfo().IsEnum)
+            #endif
+                throw new ArgumentException("T is not an enum");
+            return Enum.GetValues(typeof(T)).OfType<T>();
+        }
+
+        public static IEnumerable<int> EnumerateAllValuesAsInt<T>(this T value)
+        {
+            #if NOPCL
+            if (!typeof(T).IsEnum)
+            #else
+            if (!typeof(T).GetTypeInfo().IsEnum)
+            #endif
+                throw new ArgumentException("T is not an enum");
+            return Array.ConvertAll<T,int>(
+                Enum.GetValues(typeof(T)).OfType<T>().ToArray(), 
+                (enumValue) =>
+                {
+                    return Convert.ToInt32(enumValue);
+                });
+        }
+
+        public static IEnumerable<string> EnumerateAllNames<T>(this T value)
+        {
+            #if NOPCL
+            if (!typeof(T).IsEnum)
+            #else
+            if (!typeof(T).GetTypeInfo().IsEnum)
+            #endif
+                throw new ArgumentException("T is not an enum");
+            return Enum.GetNames(typeof(T));
+        }
+    }
+}
+
