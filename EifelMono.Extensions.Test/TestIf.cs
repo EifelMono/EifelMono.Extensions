644bceb9538acd4a0a5ddea6d21c7dd597104bce
Andreas Klapperich <eifelmono@klapperch.de>
2016-06-25 13:40:13 +0200

    IFIX: Test now works again. (Problem # in folder name)

---
+++ EifelMono.Extensions.Test/TestIf.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace EifelMono.Extensions.Test
+{
+    [TestFixture()]
+    public class TestIf
+    {
+        [Test()]
+        public  void TestIf1()
+        {
+            int i = 1;
+            int result = -1;
+
+            i.If().Is(1).Is(2).AndPush().IsIn(2, 3, 4).Then((p) =>
+                {
+                    result = 0;
+                }).Else((p) =>
+                {
+                    result = 1;
+                });
+            Assert.IsTrue(result == 1);
+
+            result = -1;
+            i.If().Is(1).Is(2).AndPush().IsIn(1, 2, 3, 4).Then((p) =>
+                {
+                    result = 0;
+                }).Else((p) =>
+                {
+                    result = 1;
+                });
+            Assert.IsTrue(result == 0);
+        }
+    }
+}
+
---
+++ EifelMono.Extensions.Test/TestMD.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+
+namespace EifelMono.Extensions.Test
+{
+    [TestFixture()]
+    public class TestMD
+    {
+        public enum StatusBarStyle
+        {
+            Normal,
+            Translucent,
+            Dark,
+            Light
+        }
+
+        [Test()]
+        public void TestMD1()
+        {
+        }
+    }
+}
+
